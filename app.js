8b6efe96a9d03ed844e5dc629237c3bda1f325c9
Dungha125 <dungha122405@gmail.com>
2025-07-20 05:29:46 +0700

    update

--- app.js
+++ app.js
@@ -9,10 +9,13 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'frontend')));
 
-app.use('/api', require('./routes/auth'));
 app.use('/api/suggestions', require('./routes/suggestions'));
 app.use('/api/admin', require('./routes/config'));
 app.use('/api/view', require('./routes/suggestions'));
+
+// Route chung chung hơn được đặt ở sau
+// Lưu ý: route này chỉ xử lý /api/login, nhưng vì cách viết nên ta phải đặt nó ở cuối
+app.use('/api', require('./routes/auth')); 
 
 // app.listen(3000, () => console.log('Server đang chạy tại http://localhost:3000'));
 module.exports = app;
